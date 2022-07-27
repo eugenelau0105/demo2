fce55f622b6473196ab23b4700a1b53e8fbe2996
Eugene <eugenelau97@ghotmail.com>
2022-07-27 22:36:28 +0800

    deletes

--- test.py
+++ test.py
@@ -3,11 +3,7 @@ from lib2to3.pgen2.pgen import DFAState
 from tkinter import DISABLED
 
 from sklearn.exceptions import DataConversionWarning
-
-
-
 print("hello world")
-
 print("next work")
 print("next work")
 
