b2764fbfe9a43763a81d004e12edddd63f3fd197
Eugene <eugenelau97@ghotmail.com>
2022-07-27 22:41:19 +0800

    add

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 
 from lib2to3.pgen2.pgen import DFAState
 from tkinter import DISABLED
+from cv2 import add
 
 from sklearn.exceptions import DataConversionWarning
 print("hello world")
 print("next work")
 print("next work")
 
-
+add
 
